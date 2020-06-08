e8fd288bb2db1a8d11f0a2dc85c7ae092eb9c3ab
Mike DelGaudio <mdelgaud@stevens.edu>
2020-06-08 18:09:04 -0400

    moving function to helper file

--- tests/gedcom_test.py
+++ tests/gedcom_test.py
@@ -2,6 +2,7 @@ import sys
 sys.path.insert(0, '../src')
 
 import main
+from helpers import dates
 
 def test_compile():
     # Can the program compile with no errors
@@ -9,7 +10,7 @@ def test_compile():
 
 
 def test_us01():
-    assert main.us01DateAfterCurrentDate("22 JUN 2019") is True
-    assert main.us01DateAfterCurrentDate("12 APR 2030") is False
-    assert main.us01DateAfterCurrentDate("18 DEC 1932") is True
-    assert main.us01DateAfterCurrentDate("N/A") is True
+    assert dates.us01DateAfterCurrentDate("22 JUN 2019") is True
+    assert dates.us01DateAfterCurrentDate("12 APR 2030") is False
+    assert dates.us01DateAfterCurrentDate("18 DEC 1932") is True
+    assert dates.us01DateAfterCurrentDate("N/A") is True
