f395836c3162353422f035d9865ef0254c2f91a2
BrandonSeidman <bseidman@stevens.edu>
2020-06-09 17:38:19 -0400

    Added function and test cases
    Added function and test cases

--- src/helpers/dates.py
+++ src/helpers/dates.py
@@ -6,7 +6,7 @@ def us01DateAfterCurrentDate(date):
     try:
         if(date == "N/A"):
             return True
-        else: 
+        else:
             #could implement check on us42 if the date is in correct format first
             strippedDate = datetime.datetime.strptime(date, "%d %b %Y")
             if(strippedDate < datetime.datetime.today()):
@@ -14,4 +14,22 @@ def us01DateAfterCurrentDate(date):
             else:
                 return False
     except ValueError:
+        print("DATE PROVIDED IS INCORRECT FORMAT")
+
+def us02BirthBeforeMarraige(birth,marriage):
+    try:
+        if(marriage == "N/A"):
+            return True
+        else:
+            bsplit = birth.split()
+            msplit = marriage.split()
+            if(int(msplit[2]) > int(bsplit[2])):
+                return True
+            elif(datetime.datetime.strptime(msplit[1], '%b').month > datetime.datetime.strptime(bsplit[1], '%b').month and int(msplit[2]) == int(bsplit[2])):
+                return True
+            elif (int(msplit[0]) > int(bsplit[0]) and datetime.datetime.strptime(msplit[1], '%b').month == datetime.datetime.strptime(bsplit[1], '%b').month and int(msplit[2]) == int(bsplit[2])):
+                return True
+            else:
+                return False
+    except ValueError: #Need us42 to check format
         print("DATE PROVIDED IS INCORRECT FORMAT")
