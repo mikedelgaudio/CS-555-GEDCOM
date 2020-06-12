c33073b2375686fe7ab418a7ae28941a59b6e373
Shannon <shobby@stevens.edu>
2020-06-12 13:05:41 -0400

    dead birthdays no longer appear

--- src/modules/list_upcoming_dates.py
+++ src/modules/list_upcoming_dates.py
@@ -3,7 +3,7 @@ from helpers import dates
 
 def birthdays(individuals):
     for x in individuals:
-        if(dates.next30days(x[3])):
+        if(dates.next30days(x[3]) and x[6] == "N/A"):
             day = x[3].split()[0]
             month = x[3].split()[1]
             print("BDAY: " + x[1] + "'s birthday is on " + month + " " + day + "!")
