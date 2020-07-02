2c384c41ab9d14e6d9272e95f91ab537ab98b6e3
Mike DelGaudio <mdelgaud@stevens.edu>
2020-07-02 16:55:31 -0400

    init

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@
 from Table import Table
 from helpers import ind as Ind, dates, fam, sorting
 import constants
-from modules import list_upcoming_dates, marriage_date_check, birth_date_check, list_deceased, unique_id, list_recent, list_living_married
+from modules import list_upcoming_dates, marriage_date_check, birth_date_check, list_deceased, unique_id, list_recent, list_living_married, order_siblings_by_age
 
 # Wrapped this in a run() function so that our pytest knows what to do
 
@@ -221,6 +221,9 @@ def run():
     
     # US30: List living married
     list_living_married.us30(individuals, families)
+    
+    
+    
     
 
 # Uncomment me for debugging!!\
