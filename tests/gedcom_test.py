6661984f8f28f448c48186b06a2589413843694f
BrandonSeidman <bseidman@stevens.edu>
2020-07-28 19:58:28 -0400

    Attempted to fix gedcom_test

--- tests/gedcom_test.py
+++ tests/gedcom_test.py
@@ -83,7 +83,6 @@ def test_us12():
     assert birth_date_check.parents_too_old("18 NOV 1999","18 NOV 1998","18 NOV 1918") is False
     assert birth_date_check.parents_too_old("18 NOV 1999","18 NOV 1938","18 NOV 1918") is False
 
-
 def test_us21():
     assert gender_check.husb_wife_gender("M","F") is True
     assert gender_check.husb_wife_gender("F","F") is False
@@ -363,6 +362,20 @@ def test_us34(capsys):
     expected = ""
     captured = capsys.readouterr()
     assert captured.out == expected
+
+def test_us13():
+    assert birth_date_check.sibling_spacing(["18 NOV 1999","18 NOV 1938","18 NOV 1918"]) is True
+    assert birth_date_check.sibling_spacing(["18 NOV 1999","18 NOV 1999","18 NOV 1918"]) is True
+    assert birth_date_check.sibling_spacing(["18 NOV 1999","19 NOV 1999","18 NOV 1918"]) is True
+    assert birth_date_check.sibling_spacing(["18 NOV 1999","17 NOV 1999","18 NOV 1918"]) is True
+    assert birth_date_check.sibling_spacing(["18 NOV 1999","17 NOV 1999","19 NOV 1999"]) is False
+    assert birth_date_check.sibling_spacing(["18 NOV 1999","17 OCT 1999","18 NOV 1918"]) is False
+    assert birth_date_check.sibling_spacing(["18 NOV 1999","18 NOV 1938","18 NOV 1918","18 NOV 1999","18 NOV 1938","18 NOV 1918","18 NOV 1999","18 NOV 1938","18 NOV 1918"]) is True
+
+def test_us15():
+    assert multiple_births.fewer_than_15_siblings(["sibling1","sibling2"]) is True
+    assert multiple_births.fewer_than_15_siblings(["sibling1","sibling2","sibling3","sibling4","sibling5","sibling6","sibling7","sibling8","sibling9","sibling10","sibling11","sibling12","sibling13","sibling14","sibling15","sibling16"]) is False
+
 
 def test_us20(capsys):
     # yes i am marrying niece/nephew
